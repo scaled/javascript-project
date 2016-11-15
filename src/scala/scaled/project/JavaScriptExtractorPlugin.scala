5fdded1aa0e35d04367366e150b96af695e6cbc4
Michael Bayne <mdb@fb.com>
2016-11-14 16:43:05 -0800

    Use project-relative file name as module path.

--- src/scala/scaled/project/JavaScriptExtractorPlugin.scala
+++ src/scala/scaled/project/JavaScriptExtractorPlugin.scala
@@ -6,6 +6,8 @@ package scaled.project
 
 import codex.extract.Extractor
 import codex.extract.TokenExtractor
+import codex.extract.Writer
+import codex.model._
 import scaled._
 
 @Plugin(tag="codex-extractor")
@@ -13,5 +15,22 @@ class JavaScriptExtractorPlugin extends ExtractorPlugin {
 
   override val suffs = Set("js")
 
-  override def extractor (project :Project, suff :String) = Some(new TokenExtractor())
+  override def extractor (project :Project, suff :String) = Some(new TokenExtractor() {
+    var rootPath = project.root.path.toString
+    override def openUnit (source :Source, writer :Writer) = {
+      val ref = super.openUnit(source, writer)
+      val fileName = source.fileName;
+      var path = source.relativePath(rootPath)
+      path = path.substring(0, math.max(0, path.length-fileName.length-1))
+      val modref = ref.plus(path);
+      writer.openDef(modref, path, Kind.MODULE, Flavor.PACKAGE, true,
+                     Access.PUBLIC, 0, 0, 0)
+      writer.emitSig(path);
+      modref
+    }
+    override def closeUnit (source :Source, writer :Writer) {
+      writer.closeDef()
+      super.closeUnit(source, writer)
+    }
+  })
 }
